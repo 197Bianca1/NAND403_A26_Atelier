d9c115353af50634fb1071fe41199509b83fb69e
william <wcoppo@etu.uqac.ca>
2026-09-21 18:11:51 -0400

    daadw

--- atelier_2/atelier_2.py
+++ atelier_2/atelier_2.py
@@ -1,14 +1,8 @@
 import sys
 import json
 
-json_file = sys.argv[1]
-print(json_file )
+with open ("drinks.json", "r") as file:
+    data = json.load(file)
 
-try:
-
- file = open(json_file)
- data = json.load(file)
- print(data)
-except:
- print(f"could not load data from {json_file}")
+print(data)
 
